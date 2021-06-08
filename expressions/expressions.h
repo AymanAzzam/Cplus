553573ac495479292b5e101dcfd2a2784c5956bd
Ayman Azzam <aymanazzam63@gmail.com>
2021-06-08 12:42:23 +0200

    refactoring & adding documentation of expressions

--- expressions/expressions.h
+++ expressions/expressions.h
@@ -14,19 +14,26 @@ class ExprNode: public Stmt{
     public:
         string name;
         
+        /**
+        * @param n: The name of variable
+        */
         ExprNode(string n = "-1") {
             name = n;
         }
         
-        virtual void execute() {
-            printf("\tPUSH\t%s\n", name.c_str());
-        };
+        /**
+        * @brief Push the variable name into the stack.
+        */
+        virtual void execute() {printf("\tPUSH\t%s\n", name.c_str());};
 };
 
 class TypeNode: public Node {
     DataType type;
 
     public:
+        /**
+        * @param t: The name of data type [_TYPE_INT, _TYPE_FLOAT, ..]
+        */
         TypeNode(DataType t) {
             type = t;
         }
@@ -39,11 +46,18 @@ class ValueNode: public ExprNode {
     DataType type;
 
     public:
+        /**
+        * @param v: The value of node
+        * @param t: The name of data type [_TYPE_INT, _TYPE_FLOAT, ..]
+        */
         ValueNode(string v, DataType t): ExprNode(v) {
             value = v;
             type = t;
         }
 
+        /**
+        * @brief Push the node value into the stack
+        */
         virtual void execute(){printf("PUSH %s\n", value.c_str());};
 };
 
@@ -51,10 +65,21 @@ class TwoOpNode: public ExprNode {
     ExprNode *left, *right;
     Operator opr;
     public:
+        /**
+        * @param l: Pointer to the left operand
+        * @param r: Pointer to the right operand
+        * @param o: The operator [_ADD, _SUB, _MUL, ..]
+        */
         TwoOpNode(ExprNode* l, ExprNode* r, Operator o);
-
+        
+        /**
+        * @brief Check the error of declaration, initialization, constant and Type mismatch
+        */
         void checkError();
-
+        
+        /**
+        * @brief Push the Two variables into the stack then apply the operation
+        */
         virtual void execute();
 
         ~TwoOpNode();
@@ -65,10 +90,20 @@ class LeftOpNode: public ExprNode {
     Operator opr;
 
     public:
+        /**
+        * @param l: Pointer to the left operand
+        * @param o: The operator [_INC_OPR, _DEC_OPR]
+        */
         LeftOpNode(ExprNode* l, Operator o);
 
+        /**
+        * @brief Check the error of declaration, initialization, constant and Type mismatch
+        */
         void checkError();
 
+        /**
+        * @brief Push the variable into the stack then apply the operation
+        */
         virtual void execute();
 
         ~LeftOpNode();
@@ -79,10 +114,20 @@ class RightOpNode: public ExprNode {
     Operator opr;
 
     public:
+        /**
+        * @param r: Pointer to the right operand
+        * @param o: The operator [_INC_OPR, _DEC_OPR, _BIT_NOT, _LOGICAL_NOT, _ADD, _SUB]
+        */
         RightOpNode(ExprNode* r, Operator o);
 
+        /**
+        * @brief Check the error of declaration, initialization, constant and Type mismatch
+        */
         void checkError();
 
+        /**
+        * @brief Push the variable into the stack then apply the operation
+        */
         virtual void execute();
 
         ~RightOpNode();
@@ -93,6 +138,9 @@ class IdentifierNode: public ExprNode {
     string name;
 
     public:
+        /**
+        * @param n: The name of identifier
+        */
         IdentifierNode(string n): ExprNode(n) {
             name = n;
         }
