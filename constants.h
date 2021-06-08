a74c2923449d48650f30cd56354f7127e0bb33a0
Ayman Azzam <aymanazzam63@gmail.com>
2021-06-08 11:43:49 +0200

    move enum operator to constants file

--- constants.h
+++ constants.h
@@ -2,6 +2,22 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
+enum Operator {
+    _MOD_EQ, _MULT_EQ, _DIV_EQ, _MINUS_EQ, _PLUS_EQ, _EQ,         
+    
+    _ADD, _SUB, _MUL, _DIV, _REM,
+
+    _LTE, _GTE, _LT, _GT, _NOT_EQ, _IS_EQ,
+    
+    _SHL, _SHR, _BIT_XOR, _BIT_OR, _BIT_AND,
+    
+    _LOGICAL_AND, _LOGICAL_OR,
+
+    _INC_OPR, _DEC_OPR,
+
+    _BIT_NOT, _LOGICAL_NOT
+};
+
 
 enum DataType
 {
--- expressions/expressions.h
+++ expressions/expressions.h
@@ -10,22 +10,6 @@
 
 using namespace std;
 
-enum Operator {
-    _MOD_EQ, _MULT_EQ, _DIV_EQ, _MINUS_EQ, _PLUS_EQ, _EQ,         
-    
-    _ADD, _SUB, _MUL, _DIV, _REM,
-
-    _LTE, _GTE, _LT, _GT, _NOT_EQ, _IS_EQ,
-    
-    _SHL, _SHR, _BIT_XOR, _BIT_OR, _BIT_AND,
-    
-    _LOGICAL_AND, _LOGICAL_OR,
-
-    _INC_OPR, _DEC_OPR,
-
-    _BIT_NOT, _LOGICAL_NOT
-};
-
 class ExprNode: public Stmt{
     public:
         string name;
